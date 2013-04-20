e7cb57365fa46557495a08fa6c6a50cca694f602
Custodius <Custodius@Gmail.com>
2013-04-20 03:09:49 -0300

    Cleaning code.

--- otherplanet.rb
+++ otherplanet.rb
@@ -2,7 +2,7 @@ class Otherplanet
   PLANET_TYPE = ["Asteroid Belt", "Desolate Rock", "Gas Giant", "Ringed Giant", "Dwarf Planet", "Ice Planet", "Volcanic Planet", "Comet", "Deralict Spacecraft", "Space Station"]
   
   def initialize 
-	@size = 3
+    @size = 3
   end
   def type
     PLANET_TYPE.at(random)
@@ -14,26 +14,16 @@ class Otherplanet
  
   def size
     case type
-	  when "Asteroid Belt" 
-	    @size = 0
-	  when "Desolate Rock" 	
-     	 @size = @size + 3
-	  when "Gas giant" 
-         @size = @size + 20
-      when "Ringed Giant" 
-         @size = @size + 15
-      when "Dwarf Planet"
-	     @size = @size + 3
-      when "Ice Planet" 
-         @size = @size + 3
-      when "Volcanic Planet"
-         @size = @size + 3
-      when "Comet"
-	     @size = 0
-	  when "Deralict Spacecraft" 
-	     @size = 0
-	  when "Space Station" 
-         @size = 0
+      when "Asteroid Belt" then @size = 0
+      when "Desolate Rock" then @size = @size + 3
+      when "Gas giant" then @size = @size + 20
+      when "Ringed Giant" then @size = @size + 15
+      when "Dwarf Planet" then @size = @size + 3
+      when "Ice Planet" then @size = @size + 3
+      when "Volcanic Planet" then @size = @size + 3
+      when "Comet" then @size = 0
+      when "Deralict Spacecraft" then @size = 0
+      when "Space Station" then @size = 0
      end	
   end
   def moon
