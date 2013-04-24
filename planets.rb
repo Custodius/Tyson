dc0960391ec95b8f0b086d10596a1479a7ca4a70
Custodius <Custodius@Gmail.com>
2013-04-24 12:37:41 -0300

    Update planets.rb

--- planets.rb
+++ planets.rb
@@ -5,6 +5,17 @@ module Tyson
       @size = random
     end
     
+    def size
+      case @size 
+      when 0..1 then "Small"
+      when 2..3 then "Medium"
+      when 4..8 then "Earth Standard"
+      when 9..11 then "Large"
+      when 12..13 then "Very Large"
+      else
+        "Error"
+    end
+    
     def random(minimum = 0, maximum = 13)
       rand(minimum..maximum)
     end
