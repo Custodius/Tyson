28c358ef0995dce384697d92d039c3a36194d502
Custodius <Custodius@Gmail.com>
2013-04-26 12:04:03 -0300

    Update planet_test.rb

--- test/planet_test.rb
+++ test/planet_test.rb
@@ -4,6 +4,6 @@ require_relative "../lib/tyson.rb"
 describe Tyson::Planet do
   it "should be setup with a distance from the sun" do
     planet = Tyson::Planet.new
-    planet.distance.must be_kind_of(Integer)
+    planet.distance.must_be_kind_of(Integer)
   end
 end
