35aad059d79f993fb1fae1ae6aeda4a65a75a095
Custodius <Custodius@Gmail.com>
2013-04-26 12:39:32 -0300

    Update planet.rb

--- lib/tyson/planet.rb
+++ lib/tyson/planet.rb
@@ -1,9 +1,10 @@
 module Tyson
   class Planet
     attr_reader :distance
-    def initialize 
+    def initialize(system) 
       @size = random
-      @distance = random
+      @system = system
+      @distance = if system.planets.any? then generate_distance else random end
     end
 
     def size
@@ -20,5 +21,23 @@ module Tyson
     def random(minimum = 0, maximum = 13)
       rand(minimum..maximum)
     end
+
+    def generate_distance
+      distances = system.planets.map { |planet| planet.distance }
+      random_distance = random 
+      while distances.include?(random_distance)
+        random_distance = random
+      end
+    end
   end
 end
+
+
+
+ #=> [5, 3, 9]
+
+
+# 0..13, that is not 5, 3, or 9
+
+
+# orbit 1 - random variable - then each planet orbit thereafter takes the sum of previous orbits and adds it to itself with a range for the next orbital body. 
